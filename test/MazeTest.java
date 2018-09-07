9bc6a24a1fb59193eb2848036afb38cb3335a96d
Manuel Strenge <manuel.strenge@eawag.ch>
2018-09-07 13:51:37 +0200

    Changed to Junit5

--- test/MazeTest.java
+++ test/MazeTest.java
@@ -1,11 +1,11 @@
 import ch.bbw.model.Maze;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
 
 public class MazeTest {
     static Maze maze;
 
-    @BeforeClass
+
     public static void testInit() {
         maze = new Maze(10);
     }
@@ -13,7 +13,7 @@ public class MazeTest {
     @Test
     public void testGet() {
         if (maze.getField(0, 0) == null) {
-            assert (false);
+
         }
     }
 }
